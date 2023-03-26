489c838933fc63e4d27c8b458bb02650d61b020f
SayedTahsin <mail.tahsin99@gmail.com>
2023-03-26 19:46:20 +0600

    25/3/2023 7:46PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -9,20 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Flutter Demo',
+      title: 'Flutter Demo',
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  // const MyHomePage({super.key, required this.title});
-
-  // final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -35,8 +32,26 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("DEMO"),
       ),
-      body: Center(
-        child: Text("NULL"),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.all(5),
+            child: Card(
+              color: Colors.amber,
+              elevation: 10,
+              margin: EdgeInsets.all(10),
+              child: Text(
+                "Chart",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Card(
+            child: Text("Transaction"),
+          ),
+        ],
       ),
     );
   }

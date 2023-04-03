74674c822343b9a7bc744528a315cdc318813512
SayedTahsin <mail.tahsin99@gmail.com>
2023-04-03 23:19:09 +0600

    chart added

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, use_key_in_widget_constructors, unused_local_variable
+
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:personal_expense_tracker/models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].dateTime.day == weekDay.day &&
+            recentTransactions[i].dateTime.month == weekDay.month &&
+            recentTransactions[i].dateTime.month == weekDay.month) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      return {
+        'day': DateFormat.E(weekDay),
+        'amount': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(children: []),
+    );
+  }
+}
